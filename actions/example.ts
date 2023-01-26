08c937d7ef318a265035631fe7783a369feb9a74
Yura Sherman <yurasherman+github@gmail.com>
2023-01-26 17:51:22 +0200

    remove ethers as it doesnt compile with tenderly

--- actions/example.ts
+++ actions/example.ts
@@ -5,7 +5,7 @@ import {
 	TransactionEvent,
 } from '@tenderly/actions';
 import axios from 'axios';
-import { ethers } from 'ethers';
+// import { ethers } from 'ethers';
 
 async function getCoinSymbol(addr: string): Promise<string> {
 	const requestURL = `https://api.coingecko.com/api/v3/coins/ethereum/contract/${addr}`;
@@ -20,5 +20,5 @@ export const aaveDepositsFn: ActionFn = async (context: Context, event: Event) =
 	let depositToken = txEvent.logs[3].address;
 	let amount = txEvent.logs[3].data;
 	let tokenSymbol = await getCoinSymbol(depositToken);
-	console.log(`Deposit of token ${tokenSymbol}, amount: ${ethers.utils.formatEther(amount)}, depositor: ${depositor}`);
+	console.log(`Deposit of token ${tokenSymbol}, amount: ${amount}, depositor: ${depositor}`);
 }
